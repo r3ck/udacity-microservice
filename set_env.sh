0613f73fd89d0d0cb2fbd62753820da2f722afa4
Pedro Reck <pmreck@gmail.com>
2022-05-09 19:59:01 -0300

    All files

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=X6Hbvb5XNx6PrDW2
+export POSTGRES_HOST=udacity-udagram.cuhs3qmucbmh.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udacity
+export AWS_BUCKET=arn:aws:s3:::udagram-076376167697-microservices
+export AWS_REGION=us-east-1
+export AWS_PROFILE=test
+export JWT_SECRET=testing
+export URL=http://localhost:8100
+
